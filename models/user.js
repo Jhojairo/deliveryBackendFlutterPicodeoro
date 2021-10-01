1ec531a7528e40af644b2aae708d3302a36b55cc
Jhojairo <jhojairomolina25@gmail.com>
2021-09-30 19:42:28 -0500

    Encryptando passord al registrar usuario

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 
 const db = require('../config/config'); // se exporta la bariable db del archivo config.js
-
+const crypto = require('crypto'),
 const User = ()=>{}; 
 
     User.getAll =  ()=>{
@@ -18,6 +18,8 @@ const User = ()=>{};
 
     
     User.create = (user)=>{
+        const myPasswordHashed = crypto.createHash('md5').update(user.password).digest('hex');
+        user.password=myPasswordHashed;
         const sql = `INSERT INTO 
         users(
             email,
